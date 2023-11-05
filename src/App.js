30e55d87ea6eb2fa668726190eb14d32b1195708
Possible <ajigbotoshojoshua2000@gmail.com>
2023-11-05 02:08:37 +0100

    create app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Landing from './page/Landing';
+import SignUp from '../src/page/Registration';
+import Login from '../src/page/Login';
+import Dashboard from './page/Dashboard';
+import 'react-toastify/dist/ReactToastify.css';
+import { ShopContextProvider } from './Context/shop-context';
+import Cart from './page/Cart';
+import Checkout from './page/Checkout';
+import CartOrder from './page/Order';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ShopContextProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='/Registration' element={<SignUp />} />
+          <Route path='/Login' element={<Login />} />
+          <Route path='/Dashboard' element={<Dashboard />} />
+          <Route path='/Cart' element={<Cart />} />
+          <Route path='/Checkout' element={<Checkout />}  />
+          <Route path='/CartOrder' element={<CartOrder />} />
+        </Routes>
+      </BrowserRouter>
+      </ShopContextProvider>
     </div>
   );
 }
