b45127fe5f774edb2d8c5b42a133e864ca12ed9f
Possible <ajigbotoshojoshua2000@gmail.com>
2023-11-05 13:27:21 +0100

    cart-items

---
+++ src/Component/Dashbord/Main/Data.js
@@ -0,0 +1,44 @@
+export const items = [
+  {
+    id: 1,
+    title: ' Stir Fry Pasta',
+    description: ' The in-house pasta and chicken by chef Moose',
+    amount: '#1000.00',
+    img: '../Image/order1.svg',
+  },
+  {
+    id: 2,
+    title: ' Stir Fry Pasta',
+    description: ' The in-house pasta and chicken by chef Moose',
+    amount: '1000.00',
+    img: '../Image/order2.svg',
+  },
+  {
+    id: 3,
+    title: ' Stir Fry Pasta',
+    description: ' The in-house pasta and chicken by chef Moose',
+    amount: '1000.00',
+    img: '../Image/order3.svg',
+  },
+  {
+    id: 4,
+    title: ' Stir Fry Pasta',
+    description: ' The in-house pasta and chicken by chef Moose',
+    amount: '1000.00',
+    img: '../Image/order4.svg',
+  },
+  {
+    id: 5,
+    title: ' Stir Fry Pasta',
+    description: ' The in-house pasta and chicken by chef Moose',
+    amount: '1000.00',
+    img: '../Image/order5.svg',
+  },
+  {
+    id: 6,
+    title: ' Stir Fry Pasta',
+    description: ' The in-house pasta and chicken by chef Moose',
+    amount: '1000.00',
+    img: '../Image/order6.svg',
+  },
+];
